bc260a3826292e931224aff9fd265112b305daae
princecalpine <78603798+princecalpine@users.noreply.github.com>
2024-10-09 23:25:08 +0530

    Update Logintest.java

--- Adminuser/src/test/java/application/Logintest.java
+++ Adminuser/src/test/java/application/Logintest.java
@@ -12,7 +12,7 @@ public class Logintest extends Base {
 	@Test
 	public static void basePageNavigation() throws IOException, InterruptedException {
 		driver = initializeDriver();
-		//testing;
+		//testingtesting;
 		driver.get("http://internal-egeria-dev-internal-789047063.us-east-1.elb.amazonaws.com/");
 		LoginPage l = new LoginPage(driver);
 		l.EmailId().sendKeys("prince.cl.ext@perkinelmer.com");
